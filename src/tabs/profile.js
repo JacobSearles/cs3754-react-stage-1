3b4d6f14a82b6866b40dc0c7a3b1ea4ca47fbdd2
jacob98 <jacob98@vt.edu>
2019-03-31 21:13:45 -0400

    profile has been material uied

--- src/tabs/profile.js
+++ src/tabs/profile.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
 
-const ProfileTab = ({ currentUser }) => {
+const styles = theme => ({
+  centered: {
+    margin: '0 auto', // https://learnlayout.com/max-width.html
+    maxWidth: 600
+  },
+  centerChildren: {
+    justifyContent: 'center'
+  },
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+});
+
+const ProfileTab = ({ currentUser, classes }) => {
   if (currentUser) {
     return (
       <>
-        <h3>Profile</h3>
-        <div className="twocols">
-          <b>username: </b>
-          <div>{currentUser.name}</div>
-          <b>ID: </b>
-          <div>{currentUser.id}</div>
-          <b>Full name: </b>
-          <div>{currentUser.fullname}</div>
-        </div>
+        <Typography align="center" variant="h5" gutterBottom>
+          Profile
+        </Typography>
+        <Card className={classes.centered}>
+          <CardContent>
+            <div className="twocols">
+              <b>username: </b>
+              <div>{currentUser.name}</div>
+              <b>ID: </b>
+              <div>{currentUser.id}</div>
+              <b>Full name: </b>
+              <div>{currentUser.fullname}</div>
+            </div>
+          </CardContent>
+        </Card>
       </>
     );
   } else {
     return (
       <>
-        <h3>Profile</h3>
-        <p>Please login to see profile information</p>
+        <Typography align="center" variant="h5" gutterBottom>
+          Please login to see profile information
+        </Typography>
       </>
     );
   }
@@ -30,4 +56,4 @@ ProfileTab.propTypes = {
   currentUser: PropTypes.object
 };
 
-export default ProfileTab;
+export default withStyles(styles)(ProfileTab);
